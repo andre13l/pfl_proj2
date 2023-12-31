096d9399e643ecfc44e01b9b9d6a92419c28317c
Rui Carvalho <ruipedrobc@gmail.com>
2023-12-31 00:23:43 +0000

    added parser tests already provided

--- compiler.hs
+++ compiler.hs
@@ -28,6 +28,7 @@ compile program = concatMap compileStm program
         compileStm NoopStm = []
 
 
+-- verify if this is according the rules defined 
 -- lexer takes a string as input and returns a list of tokens.
 -- the span is used to extract the longest prefix of characters that are not in the set " +-*;()=". This prefix is considered a token (word).
 -- The function then recursively calls itself with the remaining characters (rest) in the input string.
@@ -43,47 +44,63 @@ lexer (c:cs)
                                     in word : lexer rest
 
 
+-- este foi o gpt que fez, depois temos de ver qual deixamos ficar
+{-
+lexer :: String -> [String]
+lexer "" = []
+lexer s@(c:cs)
+  | isSpace c = lexer cs          -- Skip whitespace
+  | isAlpha c && isLower c = takeWhile isAlphaNum s : lexer (dropWhile isAlphaNum s)
+  | isDigit c = takeWhile isDigit s : lexer (dropWhile isDigit s)
+  | otherwise = [c] : lexer cs
+-}
+
+
+-- -------------------------------------------------------------
 -- need to check if this is correct
--- type Parser a = [String] -> Maybe (a, [String])
+{-
+type Parser a = [String] -> Maybe (a, [String])
 
--- parseAexp :: Parser Aexp
--- parseAexp tokens = case parseTerm tokens of
---     Just (term, "-":tokens') -> case parseAexp tokens' of
---         Just (aexp, tokens'') -> Just (SubExp term aexp, tokens'')
---         _ -> Just (term, tokens')
---     Just (term, tokens') -> Just (term, tokens')
---     _ -> Nothing
+parseAexp :: Parser Aexp
+parseAexp tokens = case parseTerm tokens of
+    Just (term, "-":tokens') -> case parseAexp tokens' of
+        Just (aexp, tokens'') -> Just (SubExp term aexp, tokens'')
+        _ -> Just (term, tokens')
+    Just (term, tokens') -> Just (term, tokens')
+    _ -> Nothing
 
--- parseTerm :: Parser Aexp
--- parseTerm (token:tokens)
---     | all isDigit token = Just (Num (read token), tokens)
---     | otherwise = Just (Var token, tokens)
+parseTerm :: Parser Aexp
+parseTerm (token:tokens)
+    | all isDigit token = Just (Num (read token), tokens)
+    | otherwise = Just (Var token, tokens)
 
--- parseBexp :: Parser Bexp
--- parseBexp tokens = case parseAexp tokens of
---     Just (aexp1, "=":tokens') -> case parseAexp tokens' of
---         Just (aexp2, tokens'') -> Just (EquExp aexp1 aexp2, tokens'')
---         _ -> Nothing
---     _ -> Nothing
+parseBexp :: Parser Bexp
+parseBexp tokens = case parseAexp tokens of
+    Just (aexp1, "=":tokens') -> case parseAexp tokens' of
+        Just (aexp2, tokens'') -> Just (EquExp aexp1 aexp2, tokens'')
+        _ -> Nothing
+    _ -> Nothing
 
--- parseStm :: Parser Stm
--- parseStm (token:":=":tokens) = case parseAexp tokens of
---     Just (aexp, tokens') -> Just (Assign token aexp, tokens')
---     _ -> Nothing
--- parseStm _ = Nothing
+parseStm :: Parser Stm
+parseStm (token:":=":tokens) = case parseAexp tokens of
+    Just (aexp, tokens') -> Just (Assign token aexp, tokens')
+    _ -> Nothing
+parseStm _ = Nothing
 
--- parseProgram :: Parser Program
--- parseProgram [] = Just ([], [])
--- parseProgram tokens = case parseStm tokens of
---     Just (stm, ";":tokens') -> case parseProgram tokens' of
---         Just (program, tokens'') -> Just (stm:program, tokens'')
---         _ -> Just ([stm], tokens')
---     Just (stm, tokens') -> Just ([stm], tokens')
---     _ -> Nothing
+parseProgram :: Parser Program
+parseProgram [] = Just ([], [])
+parseProgram tokens = case parseStm tokens of
+    Just (stm, ";":tokens') -> case parseProgram tokens' of
+        Just (program, tokens'') -> Just (stm:program, tokens'')
+        _ -> Just ([stm], tokens')
+    Just (stm, tokens') -> Just ([stm], tokens')
+    _ -> Nothing
 
--- parse :: String -> Maybe Program
--- parse = fmap fst . parseProgram . lexer
+parse :: String -> Maybe Program
+parse = fmap fst . parseProgram . lexer
+-}
 
 -- -------------------------------
 
-parse :: String -> [Stm]
+parse :: String -> Program
+
